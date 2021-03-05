672ac54db5b11308f08187cdfba6885a1da7fd5b
aharri64 <haveacompleteday@gmail.com>
2021-03-05 15:18:17 -0700

    sprint 3: completed

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,11 +1,23 @@
 import { useState, useEffect } from 'react';
+import TodoModel from '../models/Todo'
+import Todos from '../components/Todos'
 
-const TodosContainer = () => {
+export default function TodosContainer(){
+const [todos, setTodos] = useState([])
+
+    useEffect(()=> {
+        const fetchData = async()=> {
+            const res = await TodoModel.all()
+            console.log(res)
+            setTodos(res.data)
+        }
+        fetchData()
+    })
     return (
         <div className="todosContainer">
+            <Todos todos={todos} />
             <h1>Todos  Container</h1>
         </div>
     );
 }
 
-export default TodosContainer;
---
+++ src/models/Todo.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const endPoint = `https://sei-111-todo-backend.herokuapp.com/todos`;
+
+// router.get('/', (req,res) => 
+
+// * define a regular className
+class TodoModel {
+    //is define a method to get all todos
+    static all = () => {
+        let request = axios.get(endPoint);
+        // this is going to take time, it is a promise
+        return request
+    }
+    // methods to update delete etc.
+}
+
+export default TodoModel
